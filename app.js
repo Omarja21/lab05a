d177956d203f5ca1023f135437918dace4c778ec
Omarja21 <jarrahom21@gmail.com>
2021-03-06 15:40:38 +0200

    solved the forth problem

--- app.js
+++ app.js
@@ -108,15 +108,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 
 function multiplyArray(multArr) { //eslint-disable-line
-    multArr=[2,3,4];
-let multiArr=multArr[0]*multArr[1]*multArr[2]
-let stringArr='The numbers '+ multArr+' have a product of '+multiArr+'.'
-let final=[multiArr, stringArr]
-return final;
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
